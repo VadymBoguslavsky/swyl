af49594e3dcc2909eca8116728e6b24827fba383
VadymBoguslavsky <axixe92@gmail.com>
2019-02-06 01:44:41 +0200

    menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,3 +9,15 @@
       }
     })
 })();
+(function () {
+  var main = document.querySelectorAll('.menu__list .list__item a');
+  for(var i = 0; i<main.length; i++){
+    var all = main[i];
+    all.addEventListener('click', function (e) {
+      var remove = document.querySelector('.list__active')
+      remove.classList.remove('list__active')
+      e.currentTarget.classList.add('list__active')
+    })
+  }
+})();
+
