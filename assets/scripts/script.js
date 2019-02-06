8a75b699db5f62b8ab8a570b182f6f8ca2f113d6
VadymBoguslavsky <axixe92@gmail.com>
2019-02-06 12:31:41 +0200

    classnames

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 (function () {
-  var main = document.querySelector('.user__report');
+  var main = document.querySelector('.user-report');
     main.addEventListener('click', function () {
-      var lol = document.querySelector('.user__hidden');
+      var lol = document.querySelector('.user-hidden');
       if(lol.style.display==='none'){
         lol.style.display="flex"
       } else{
@@ -10,13 +10,13 @@
     })
 })();
 (function () {
-  var main = document.querySelectorAll('.menu__list .list__item a');
+  var main = document.querySelectorAll('.menu-list .menu-list__item a');
   for(var i = 0; i<main.length; i++){
     var all = main[i];
     all.addEventListener('click', function (e) {
-      var remove = document.querySelector('.list__active')
-      remove.classList.remove('list__active')
-      e.currentTarget.classList.add('list__active')
+      var remove = document.querySelector('.menu-list__active')
+      remove.classList.remove('menu-list__active')
+      e.currentTarget.classList.add('menu-list__active')
     })
   }
 })();
